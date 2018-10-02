aff2335dd5e43bb900f25659308e6339700fe76f
Alyssa Alecci <Alyssa@Alyssas-iMac.local>
2018-10-02 16:23:20 -0400

    portrait image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,11 @@ import React from "react";
 const About = () => (
     <div className="postcontent center">
         <div className="postheader">About</div>
+
+    <div className="portrait">
+    <img src="assets/images/layout/portrait.jpg" alt="portrait"/>
+    </div>
+
         <div className="text">
             <p>Alyssa Alecci is a scholar of visual design who spends her time coding, drawing, and daydreaming about drawing and coding. With a passion for creating, brainstorming, and giving back to her communities, she strives to create polished and intuitive work and a great working environment for those around her.
     </p>
