26c43ed2e7694082fe9647433c981a452310e810
Alyssa Alecci <Alyssa@Alyssas-iMac.local>
2018-12-05 14:42:30 -0500

    errors fixed 2

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,92 +10,92 @@ const Portfolio = () => (
             <div className="portfolio_index">
 
                 <div className="portfolio_item">
-                    <div class="folio-img-wrapper">
+                    <div className="folio-img-wrapper">
                         <div className="title">'Let's Hang, Man!'</div>
                         <a href="https://ayemae.github.io/hangman-game/" target="_blank" rel="noopener noreferrer" >
                             <img src="assets/images/portfolio/Ptfo-hangman.png" alt="Hangman" />
                         </a>
                     </div>
                     <a className="see-code" href="https://github.com/Ayemae/hangman-game" target="_blank" rel="noopener noreferrer" >
-                        <i class="fab fa-github"></i> See code
+                        <i className="fab fa-github"></i> See code
                 </a>
                     <p>Figure out what your plans are before your schedule fills up! Made with Javascript.</p>
                 </div>
 
                 <div className="portfolio_item">
-                    <div class="folio-img-wrapper">
+                    <div className="folio-img-wrapper">
                         <div className="title">'Right Angles'</div>
                         <a href="https://ayemae.github.io/RightAngles/" target="_blank" rel="noopener noreferrer" >
                             <img src="assets/images/portfolio/Ptfo-Right-Angles.png" alt="RightAngles" />
                         </a>
                     </div>
                     <a className="see-code" href="https://github.com/Ayemae/RightAngles" target="_blank" rel="noopener noreferrer" >
-                        <i class="fab fa-github"></i> See code
+                        <i className="fab fa-github"></i> See code
                 </a>
                     <p>A tile rotation game made with Javascript.</p>
                 </div>
 
                 <div className="portfolio_item">
-                    <div class="folio-img-wrapper">
+                    <div className="folio-img-wrapper">
                         <div className="title">'Weird Wide Web'</div>
                         <a href="https://guarded-meadow-97753.herokuapp.com/" target="_blank" rel="noopener noreferrer" >
                             <img src="assets/images/portfolio/Ptfo-weirdwideweb.png" alt="WeirdWideWeb" />
                         </a>
                     </div>
                     <a className="see-code" href="https://github.com/Uncanny-Team/Weird-Wide-Web" target="_blank" rel="noopener noreferrer" >
-                        <i class="fab fa-github"></i> See code
+                        <i className="fab fa-github"></i> See code
                 </a>
                     <p>A database of weird miscellanea that users can submit and add to. Collaborative project in which I was responsible for front-end.</p>
                 </div>
 
                 <div className="portfolio_item">
-                    <div class="folio-img-wrapper">
+                    <div className="folio-img-wrapper">
                         <div className="title">'Giphy App'</div>
                         <a href="https://ayemae.github.io/GiphyApp/" target="_blank" rel="noopener noreferrer" >
                             <img src="assets/images/portfolio/Ptfo-Giphy.png" alt="GiphyApp" />
                         </a>
                     </div>
                     <a className="see-code" href="https://github.com/Ayemae/GiphyApp" target="_blank" rel="noopener noreferrer" >
-                        <i class="fab fa-github"></i> See code
+                        <i className="fab fa-github"></i> See code
                 </a>
                     <p>Spawn gifs from the Giphy API!</p>
                 </div>
 
                 <div className="portfolio_item">
-                    <div class="folio-img-wrapper">
+                    <div className="folio-img-wrapper">
                         <div className="title">'Train Scheduler'</div>
                         <a href="https://ayemae.github.io/Train-Scheduler/" target="_blank" rel="noopener noreferrer" >
                             <img src="assets/images/portfolio/Ptfo-Trains.png" alt="Train Scheduler" />
                         </a>
                     </div>
                     <a className="see-code" href="https://github.com/Ayemae/Train-Scheduler" target="_blank" rel="noopener noreferrer" >
-                        <i class="fab fa-github"></i> See code
+                        <i className="fab fa-github"></i> See code
                 </a>
                     <p>Add trains and the form will calculate their times.</p>
                 </div>
 
                 <div className="portfolio_item">
-                    <div class="folio-img-wrapper">
+                    <div className="folio-img-wrapper">
                         <div className="title">'Liri.js'</div>
                         <a href="https://github.com/Ayemae/liri-node-app" target="_blank" rel="noopener noreferrer" >
                             <img src="assets/images/portfolio/Ptfo-liri.png" alt="liri" />
                         </a>
                     </div>
                     <a className="see-code" href="https://github.com/Ayemae/liri-node-app" target="_blank" rel="noopener noreferrer" >
-                        <i class="fab fa-github"></i> See code
+                        <i className="fab fa-github"></i> See code
                 </a>
                     <p>Non-deployed; console only. Made with Node.js and APIs that can return song, movie, and Twitter data.</p>
                 </div>
 
                 <div className="portfolio_item">
-                    <div class="folio-img-wrapper">
+                    <div className="folio-img-wrapper">
                         <div className="title">'Node Hang-Man'</div>
                         <a href="https://github.com/Ayemae/word-guess" target="_blank" rel="noopener noreferrer" >
                             <img src="assets/images/portfolio/Ptfo-word-guess.png" alt="Console Hangman" />
                         </a>
                     </div>
                     <a className="see-code" href="https://github.com/Ayemae/word-guess" target="_blank" rel="noopener noreferrer" >
-                        <i class="fab fa-github"></i> See code
+                        <i className="fab fa-github"></i> See code
                 </a>
                     <p>Non-deployed; console only. A hang-man game made with Node inquirer.</p>
                 </div>
@@ -105,14 +105,12 @@ const Portfolio = () => (
 
 
 
-
-
             <h1 id="htmlcss">HTML/CSS</h1>
 
             <div className="portfolio_index">
 
                 <div className="portfolio_item">
-                    <div class="folio-img-wrapper">
+                    <div className="folio-img-wrapper">
                         <div className="title">'Comic CSS Templates'</div>
                         <a href="http://propandatemplates.spiderforest.com/List/TemplateWelcome/TemplateWelcome.html" target="_blank" rel="noopener noreferrer" >
                             <img src="assets/images/portfolio/Ptfo-templates.jpg" alt="templates" />
@@ -121,20 +119,8 @@ const Portfolio = () => (
                     <p>A variety of CSS templates I made for my community's homebrew comic publishing CMS.</p>
                 </div>
 
-
-
-            <div className="portfolio_item">
-                <div class="folio-img-wrapper">
-                    <div className="title">SF Forum</div>
-                    <a href="http://forums.spiderforest.net/" target="_blank" rel="noopener noreferrer" >
-                        <img src="assets/images/portfolio/Ptfo-SFForum.png" alt="SpiderForest Forums" />
-                    </a>
-                </div>
-                <p>SpiderForest Community Forum, designed to compliment the main site design. Click to view site.</p>
-            </div>
-
                 <div className="portfolio_item">
-                    <div class="folio-img-wrapper">
+                    <div className="folio-img-wrapper">
                         <div className="title">'Lapse'</div>
                         <a href="http://www.lapsecomic.com" target="_blank" rel="noopener noreferrer" >
                             <img src="assets/images/portfolio/Ptfo-Lapse.png" alt="Lapse" />
@@ -144,7 +130,17 @@ const Portfolio = () => (
                 </div>
 
                 <div className="portfolio_item">
-                    <div class="folio-img-wrapper">
+                    <div className="folio-img-wrapper">
+                        <div className="title">SF Forum</div>
+                        <a href="http://forums.spiderforest.net/" target="_blank" rel="noopener noreferrer" >
+                            <img src="assets/images/portfolio/Ptfo-SFForum.png" alt="SpiderForest Forums" />
+                        </a>
+                    </div>
+                    <p>SpiderForest Community Forum, designed to compliment the main site design. Click to view site.</p>
+                </div>
+
+                <div className="portfolio_item">
+                    <div className="folio-img-wrapper">
                         <div className="title">'DDWG'</div>
                         <a href="http://www.damsels-dont-wear-glasses.com" target="_blank" rel="noopener noreferrer" >
                             <img src="assets/images/portfolio/Ptfo-DDWG.png" alt="DDWG" />
@@ -154,7 +150,7 @@ const Portfolio = () => (
                 </div>
 
                 <div className="portfolio_item">
-                    <div class="folio-img-wrapper">
+                    <div className="folio-img-wrapper">
                         <div className="title">'Sombulus'</div>
                         <a href="http://www.sombulus.com" target="_blank" rel="noopener noreferrer" >
                             <img src="assets/images/portfolio/Ptfo-Sombulus.png" alt="Sombulus" />
@@ -168,6 +164,6 @@ const Portfolio = () => (
 
         </div>
     </div>
-        );
+);
 
 export default Portfolio;
