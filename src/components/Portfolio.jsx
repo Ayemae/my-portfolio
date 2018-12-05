4bdf4b0be94c0dbe109ebb90a6b717bf6ede9471
Alyssa Alecci <Alyssa@Alyssas-iMac.local>
2018-12-05 14:31:24 -0500

    errors fixed

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,154 +6,170 @@ const Portfolio = () => (
         <div className="text">
 
             <h1>Development</h1>
-            
-        <div className="portfolio_index">
 
-            <div className="portfolio_item">
-                <div class="folio-img-wrapper">
-                <div className="title">'Let's Hang, Man!'</div>
-                    <a href="https://ayemae.github.io/hangman-game/" target="_blank" rel="noopener noreferrer" >
-                        <img src="assets/images/portfolio/Ptfo-hangman.png" alt="Hangman" />
-                    </a>
-                </div>
-                <a className="see-code" href="https://github.com/Ayemae/hangman-game" target="_blank" rel="noopener noreferrer" >
-                    <i class="fab fa-github"></i> See code
+            <div className="portfolio_index">
+
+                <div className="portfolio_item">
+                    <div class="folio-img-wrapper">
+                        <div className="title">'Let's Hang, Man!'</div>
+                        <a href="https://ayemae.github.io/hangman-game/" target="_blank" rel="noopener noreferrer" >
+                            <img src="assets/images/portfolio/Ptfo-hangman.png" alt="Hangman" />
+                        </a>
+                    </div>
+                    <a className="see-code" href="https://github.com/Ayemae/hangman-game" target="_blank" rel="noopener noreferrer" >
+                        <i class="fab fa-github"></i> See code
                 </a>
-                <p>Figure out what your plans are before your schedule fills up! Made with Javascript.</p>
+                    <p>Figure out what your plans are before your schedule fills up! Made with Javascript.</p>
+                </div>
+
+                <div className="portfolio_item">
+                    <div class="folio-img-wrapper">
+                        <div className="title">'Right Angles'</div>
+                        <a href="https://ayemae.github.io/RightAngles/" target="_blank" rel="noopener noreferrer" >
+                            <img src="assets/images/portfolio/Ptfo-Right-Angles.png" alt="RightAngles" />
+                        </a>
+                    </div>
+                    <a className="see-code" href="https://github.com/Ayemae/RightAngles" target="_blank" rel="noopener noreferrer" >
+                        <i class="fab fa-github"></i> See code
+                </a>
+                    <p>A tile rotation game made with Javascript.</p>
+                </div>
+
+                <div className="portfolio_item">
+                    <div class="folio-img-wrapper">
+                        <div className="title">'Weird Wide Web'</div>
+                        <a href="https://guarded-meadow-97753.herokuapp.com/" target="_blank" rel="noopener noreferrer" >
+                            <img src="assets/images/portfolio/Ptfo-weirdwideweb.png" alt="WeirdWideWeb" />
+                        </a>
+                    </div>
+                    <a className="see-code" href="https://github.com/Uncanny-Team/Weird-Wide-Web" target="_blank" rel="noopener noreferrer" >
+                        <i class="fab fa-github"></i> See code
+                </a>
+                    <p>A database of weird miscellanea that users can submit and add to. Collaborative project in which I was responsible for front-end.</p>
+                </div>
+
+                <div className="portfolio_item">
+                    <div class="folio-img-wrapper">
+                        <div className="title">'Giphy App'</div>
+                        <a href="https://ayemae.github.io/GiphyApp/" target="_blank" rel="noopener noreferrer" >
+                            <img src="assets/images/portfolio/Ptfo-Giphy.png" alt="GiphyApp" />
+                        </a>
+                    </div>
+                    <a className="see-code" href="https://github.com/Ayemae/GiphyApp" target="_blank" rel="noopener noreferrer" >
+                        <i class="fab fa-github"></i> See code
+                </a>
+                    <p>Spawn gifs from the Giphy API!</p>
+                </div>
+
+                <div className="portfolio_item">
+                    <div class="folio-img-wrapper">
+                        <div className="title">'Train Scheduler'</div>
+                        <a href="https://ayemae.github.io/Train-Scheduler/" target="_blank" rel="noopener noreferrer" >
+                            <img src="assets/images/portfolio/Ptfo-Trains.png" alt="Train Scheduler" />
+                        </a>
+                    </div>
+                    <a className="see-code" href="https://github.com/Ayemae/Train-Scheduler" target="_blank" rel="noopener noreferrer" >
+                        <i class="fab fa-github"></i> See code
+                </a>
+                    <p>Add trains and the form will calculate their times.</p>
+                </div>
+
+                <div className="portfolio_item">
+                    <div class="folio-img-wrapper">
+                        <div className="title">'Liri.js'</div>
+                        <a href="https://github.com/Ayemae/liri-node-app" target="_blank" rel="noopener noreferrer" >
+                            <img src="assets/images/portfolio/Ptfo-liri.png" alt="liri" />
+                        </a>
+                    </div>
+                    <a className="see-code" href="https://github.com/Ayemae/liri-node-app" target="_blank" rel="noopener noreferrer" >
+                        <i class="fab fa-github"></i> See code
+                </a>
+                    <p>Non-deployed; console only. Made with Node.js and APIs that can return song, movie, and Twitter data.</p>
+                </div>
+
+                <div className="portfolio_item">
+                    <div class="folio-img-wrapper">
+                        <div className="title">'Node Hang-Man'</div>
+                        <a href="https://github.com/Ayemae/word-guess" target="_blank" rel="noopener noreferrer" >
+                            <img src="assets/images/portfolio/Ptfo-word-guess.png" alt="Console Hangman" />
+                        </a>
+                    </div>
+                    <a className="see-code" href="https://github.com/Ayemae/word-guess" target="_blank" rel="noopener noreferrer" >
+                        <i class="fab fa-github"></i> See code
+                </a>
+                    <p>Non-deployed; console only. A hang-man game made with Node inquirer.</p>
+                </div>
             </div>
-
-            <div className="portfolio_item">
-                <div class="folio-img-wrapper">
-                    <div className="title">'Right Angles'</div>
-                    <a href="https://ayemae.github.io/RightAngles/" target="_blank" rel="noopener noreferrer" >
-                        <img src="assets/images/portfolio/Ptfo-Right-Angles.png" alt="RightAngles" />
-                    </a>
-                </div>
-                <a className="see-code" href="https://github.com/Ayemae/RightAngles" target="_blank" rel="noopener noreferrer" >
-                    <i class="fab fa-github"></i> See code
-                </a>
-                <p>A tile rotation game made with Javascript.</p>
-            </div>
-
-            <div className="portfolio_item">
-                <div class="folio-img-wrapper">
-                    <div className="title">'Weird Wide Web'</div>
-                    <a href="https://guarded-meadow-97753.herokuapp.com/" target="_blank" rel="noopener noreferrer" >
-                        <img src="assets/images/portfolio/Ptfo-WeirdWideWeb.png" alt="WeirdWideWeb" />
-                    </a>
-                </div>
-                <a className="see-code" href="https://github.com/Uncanny-Team/Weird-Wide-Web" target="_blank" rel="noopener noreferrer" >
-                    <i class="fab fa-github"></i> See code
-                </a>
-                <p>A database of weird miscellanea that users can submit and add to. Collaborative project in which I was responsible for front-end.</p>
-            </div>
-
-            <div className="portfolio_item">
-                <div class="folio-img-wrapper">
-                    <div className="title">'Giphy App'</div>
-                    <a href="https://ayemae.github.io/GiphyApp/" target="_blank" rel="noopener noreferrer" >
-                        <img src="assets/images/portfolio/Ptfo-Giphy.png" alt="GiphyApp" />
-                    </a>
-                </div>
-                <a className="see-code" href="https://github.com/Ayemae/GiphyApp" target="_blank" rel="noopener noreferrer" >
-                    <i class="fab fa-github"></i> See code
-                </a>
-                <p>Spawn gifs from the Giphy API!</p>
-    </div>
-
-            <div className="portfolio_item">
-                <div class="folio-img-wrapper">
-                    <div className="title">'Train Scheduler'</div>
-                    <a href="https://ayemae.github.io/Train-Scheduler/" target="_blank" rel="noopener noreferrer" >
-                        <img src="assets/images/portfolio/Ptfo-Trains.png" alt="Train Scheduler" />
-                    </a>
-                </div>
-                <a className="see-code" href="https://github.com/Ayemae/Train-Scheduler" target="_blank" rel="noopener noreferrer" >
-                    <i class="fab fa-github"></i> See code
-                </a>
-                <p>Add trains and the form will calculate their times.</p>
-    </div>
-
-            <div className="portfolio_item">
-                <div class="folio-img-wrapper">
-                    <div className="title">'Liri.js'</div>
-                    <a href="https://github.com/Ayemae/liri-node-app" target="_blank" rel="noopener noreferrer" >
-                        <img src="assets/images/portfolio/Ptfo-liri.png" alt="liri" />
-                    </a>
-                </div>
-                <a className="see-code" href="https://github.com/Ayemae/liri-node-app" target="_blank" rel="noopener noreferrer" >
-                    <i class="fab fa-github"></i> See code
-                </a>
-                <p>Non-deployed; console only. Made with Node.js and APIs that can return song, movie, and Twitter data.</p>
-    </div>
-
-            <div className="portfolio_item">
-                <div class="folio-img-wrapper">
-                    <div className="title">'Node Hang-Man'</div>
-                    <a href="https://github.com/Ayemae/word-guess" target="_blank" rel="noopener noreferrer" >
-                        <img src="assets/images/portfolio/Ptfo-word-guess.png" alt="Console Hangman" />
-                    </a>
-                </div>
-                <a className="see-code" href="https://github.com/Ayemae/word-guess" target="_blank" rel="noopener noreferrer" >
-                    <i class="fab fa-github"></i> See code
-                </a>
-                <p>Non-deployed; console only. A hang-man game made with Node inquirer.</p>
-    </div>
-    </div>
 
             <br className="clear" />
 
 
 
-            <h1>CSS</h1>
+            <h1 id="htmlcss">HTML/CSS</h1>
 
-        <div className="portfolio_index">
+            <div className="portfolio_index">
 
-            <div className="portfolio_item">
-                <div class="folio-img-wrapper">
-                    <div className="title">'Lapse'</div>
-                    <a href="http://www.lapsecomic.com" target="_blank" rel="noopener noreferrer" >
-                        <img src="assets/images/portfolio/Ptfo-Lapse.png" alt="Lapse" />
-                    </a>
+                <div className="portfolio_item">
+                    <div class="folio-img-wrapper">
+                        <div className="title">'Comic CSS Templates'</div>
+                        <a href="http://propandatemplates.spiderforest.com/List/TemplateWelcome/TemplateWelcome.html" target="_blank" rel="noopener noreferrer" >
+                            <img src="assets/images/portfolio/Ptfo-templates.jpg" alt="templates" />
+                        </a>
+                    </div>
+                    <p>A variety of CSS templates I made for my community's homebrew comic publishing CMS.</p>
                 </div>
-                <p>My own comic, on my own site. Click to view site.</p>
-</div>
 
-            <div className="portfolio_item">
-                <div class="folio-img-wrapper">
-                    <div className="title">SF Forum</div>
-                    <a href="http://forums.spiderforest.net/" target="_blank" rel="noopener noreferrer" >
-                        <img src="assets/images/portfolio/Ptfo-SFForum.png" alt="SpiderForest Forums" />
-                    </a>
+
+
+                <div className="portfolio_item">
+                    <div class="folio-img-wrapper">
+                        <div className="title">SF Forum</div>
+                        <a href="http://forums.spiderforest.net/" target="_blank" rel="noopener noreferrer" >
+                            <img src="assets/images/portfolio/Ptfo-SFForum.png" alt="SpiderForest Forums" />
+                        </a>
+                    </div>
+                    <p>SpiderForest Community Forum, designed to compliment the main site design. Click to view site.</p>
                 </div>
-                <p>SpiderForest Community Forum, designed to compliment the main site design. Click to view site.</p>
-</div>
 
-            <div className="portfolio_item">
-                <div class="folio-img-wrapper">
-                    <div className="title">'DDWG'</div>
-                    <a href="http://www.damsels-dont-wear-glasses.com" target="_blank" rel="noopener noreferrer" >
-                        <img src="assets/images/portfolio/Ptfo-DDWG.png" alt="DDWG" />
-                    </a>
+
+                <div className="portfolio_index">
+
+                    <div className="portfolio_item">
+                        <div class="folio-img-wrapper">
+                            <div className="title">'Lapse'</div>
+                            <a href="http://www.lapsecomic.com" target="_blank" rel="noopener noreferrer" >
+                                <img src="assets/images/portfolio/Ptfo-Lapse.png" alt="Lapse" />
+                            </a>
+                        </div>
+                        <p>My own comic, on my own site. Click to view site.</p>
+                    </div>
+
+                    <div className="portfolio_item">
+                        <div class="folio-img-wrapper">
+                            <div className="title">'DDWG'</div>
+                            <a href="http://www.damsels-dont-wear-glasses.com" target="_blank" rel="noopener noreferrer" >
+                                <img src="assets/images/portfolio/Ptfo-DDWG.png" alt="DDWG" />
+                            </a>
+                        </div>
+                        <p>Site design for webcomic, done with the comic author's input. Click to view site.</p>
+                    </div>
+
+                    <div className="portfolio_item">
+                        <div class="folio-img-wrapper">
+                            <div className="title">'Sombulus'</div>
+                            <a href="http://www.sombulus.com" target="_blank" rel="noopener noreferrer" >
+                                <img src="assets/images/portfolio/Ptfo-Sombulus.png" alt="Sombulus" />
+                            </a>
+                        </div>
+                        <p>Site design for webcomic, done with the comic author's input. Click to view site.</p>
+                    </div>
+
                 </div>
-                <p>Site design for webcomic, done with the comic author's input. Click to view site.</p>
-</div>
+                <br className="clear" />
 
-            <div className="portfolio_item">
-                <div class="folio-img-wrapper">
-                    <div className="title">'Sombulus'</div>
-                    <a href="http://www.sombulus.com" target="_blank" rel="noopener noreferrer" >
-                        <img src="assets/images/portfolio/Ptfo-Sombulus.png" alt="Sombulus" />
-                    </a>
-                </div>
-                <p>Site design for webcomic, done with the comic author's input. Click to view site.</p>
-</div>
-
+            </div>
         </div>
-            <br className="clear" />
-
         </div>
-    </div>
-);
-
+        );
+        
 export default Portfolio;
