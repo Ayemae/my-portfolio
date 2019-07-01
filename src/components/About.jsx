819daaf30d1be20517b8436d7c444666397b252c
Alyssa Alecci <Alyssa@Alyssas-iMac.fios-router.home>
2019-06-30 22:20:17 -0400

    Updating

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,6 +12,8 @@ const About = () => (
             <p>Alyssa Alecci is a scholar of visual design who spends her time coding, drawing, and daydreaming about drawing and coding. With a passion for creating, brainstorming, and giving back to her communities, she strives to create polished and intuitive work and a great working environment for those around her.
     </p>
             <p>She loves teaching, storytelling, and editing, and has aided a number of her creative peers in their endeavors. In her spare time, she writes and draws a drama/horror webcomic called <a href="http://lapsecomic.com">Lapse</a>, and does volunteer administrative duties and web design for the SpiderForest Webcomic Collective.</p>
+
+        <p><a href="assets/2019esume.pdf">See my resume (PDF)</a></p>
         </div>
         <br className="clear" />
     </div>
